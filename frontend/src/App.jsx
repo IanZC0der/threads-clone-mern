e78ba7766c2f85471b3023cc2fd68cf1c65f13ae
Ian Zhang <benchi2@pdx.edu>
2023-11-19 16:45:15 -0600

    added logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import { Button } from "@chakra-ui/react"
+// import { Button } from "@chakra-ui/react"
 import { Container } from '@chakra-ui/react'
+import { Route, Routes } from "react-router-dom"
+import UserPage from './pages/UserPage'
+import PostPage from './pages/PostPage'
+import Header from './components/Header'
+
 
 function App() {
 
   return (
     <Container maxW="620px">
-      <Button>Test</Button>
+      <Header />
+      <Routes>
+        <Route path="/:username" element={<UserPage />}/>
+        <Route path="/:username/post/:pid" element={<PostPage />}/>
+
+      </Routes>
     </Container>
   )
 }
