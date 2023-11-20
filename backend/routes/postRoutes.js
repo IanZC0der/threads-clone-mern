c01f0131dd253e2b4b2bb3e7119cab6389805b9b
Ian Zhang <benchi2@pdx.edu>
2023-11-20 15:32:00 -0600

    created reply controller

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import protectRoute from "../middleware/protectRoute.js"
-import { createPost, getPost, deletePost, likeUnlikePost} from '../controllers/postController.js'
+import { createPost, getPost, deletePost, likeUnlikePost, replyToPost} from '../controllers/postController.js'
 
 const router = express.Router()
 
@@ -9,6 +9,7 @@ router.post("/create", protectRoute, createPost)
 router.get("/:id", getPost)
 router.delete("/:id", protectRoute, deletePost)
 router.post("/like/:id", protectRoute, likeUnlikePost)
+router.post("/reply/:id", protectRoute, replyToPost)
 
 
 export default router
