1bf240f529ccc7074157c0c1bbd10bb8736ec55b
Ian Zhang <benchi2@pdx.edu>
2023-11-20 15:20:13 -0600

    created likepost controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -95,4 +95,43 @@ const deletePost = async (req, res) => {
 		res.status(500).json({ error: err.message });
 	}
 }
-export { createPost, getPost, deletePost}
+
+/**
+ * 1. find post with the given id
+ * 2. check if post exists
+ * 3. check if user already liked the post
+ * 4. if user already liked the post, unlike post
+ * 5. if user has not liked the post, like post
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+const likeUnlikePost = async (req, res) => {
+    try {
+		const { id: postId } = req.params
+		const userId = req.user._id
+
+		const post = await Post.findById(postId)
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" })
+		}
+
+		const userLikedPost = post.likes.includes(userId)
+
+        // if user already liked the post, unlike post
+        // if user has not liked the post, like post
+		if (userLikedPost) {
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
+			res.status(200).json({ message: "Post unliked successfully" })
+		} else {
+			post.likes.push(userId)
+			await post.save()
+			res.status(200).json({ message: "Post liked successfully" })
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+    
+}
+export { createPost, getPost, deletePost, likeUnlikePost}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 
 import protectRoute from "../middleware/protectRoute.js"
-import { createPost, getPost, deletePost} from '../controllers/postController.js'
+import { createPost, getPost, deletePost, likeUnlikePost} from '../controllers/postController.js'
 
 const router = express.Router()
 
 router.post("/create", protectRoute, createPost)
 router.get("/:id", getPost)
 router.delete("/:id", protectRoute, deletePost)
+router.post("/like/:id", protectRoute, likeUnlikePost)
 
 
 export default router
