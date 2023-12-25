5183415f4982910570a98cb328b513253ad422f5
Ian Zhang <benchi2@pdx.edu>
2023-12-24 18:40:51 -0600

    fixed a few bugs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,17 +6,26 @@ import { RxAvatar } from "react-icons/rx"
 import { Link as RouterLink } from "react-router-dom"
 import { Button } from "@chakra-ui/react"
 import { FiLogOut } from "react-icons/fi"
-
+import useLogout from "../../hooks/useLogout"
+import authScreenAtom from "../atom/authAtom"
+import { useSetRecoilState } from "recoil"
 
 const Header = () => {
     const user = useRecoilValue(userAtom)
     const {colorMode, toggleColorMode} = useColorMode()
+    const Logout = useLogout()
+    const setAuthScreen = useSetRecoilState(authScreenAtom)
     return (<Flex justifyContent={"space-between"} mt={6} mb={12}>
             {user && (
 				<Link as={RouterLink} to='/'>
 					<AiFillHome size={24} />
 				</Link>
 			)}
+            {!user && (
+                <Link as={RouterLink} to={"/auth"} onClick={()=>{setAuthScreen("login")}}>
+                    Login
+                </Link>
+            )}
         <Image 
             cursor={"pointer"}
             alt="logo"
@@ -30,10 +39,15 @@ const Header = () => {
 					<Link as={RouterLink} to={`/${user.username}`}>
 						<RxAvatar size={24} />
 					</Link>
-                    <Button size={"sm"} >
+                    <Button size={"xs"} onClick={Logout}>
                     <FiLogOut size={20} />
                     </Button>
 				</Flex>
+			)}
+        {!user && (
+				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")}>
+					Sign up
+				</Link>
 			)}
         </Flex>)
 }
