d39e3f0bee1955f576b6e3e69e3ed9dcbed61198
Ian Zhang <benchi2@pdx.edu>
2023-12-25 14:19:19 -0600

    created conversation model and message model

---
+++ backend/models/messageModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+const messageSchema = mongoose.Schema({
+    conversationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Conversation",
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    text: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Message = mongoose.model('Message', messageSchema)
+export default Message
