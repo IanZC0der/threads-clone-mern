cdf573c237f561a5ecbc71794a6716bd0912a71d
Ian Zhang <benchi2@pdx.edu>
2023-12-25 18:28:33 -0600

    created getConversations method

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {sendMessage, getMessages} from '../controllers/messageController.js'
+import {sendMessage, getMessages, getConversations} from '../controllers/messageController.js'
 import protectRoute from '../middleware/protectRoute.js'
 const router = express.Router()
 
+router.get("/conversations", protectRoute, getConversations)
 router.post("/", protectRoute, sendMessage)
 router.get("/:otherUserId", protectRoute, getMessages)
 
